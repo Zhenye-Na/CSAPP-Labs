1254efd2ea0dfedf2bf6476111dc56c001a9877a
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-03-26 12:32:22 -0500

    Initial commit

---
+++ Deadline Tracker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Deadline Tracker
+//
+//  Created by Zayne Na on 3/26/18.
+//  Copyright © 2018 iNa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MasterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.swift
+//  Deadline Tracker
+//
+//  Created by Zayne Na on 3/26/18.
+//  Copyright © 2018 iNa. All rights reserved.
+//
+
+import Cocoa
+
+class MasterViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
