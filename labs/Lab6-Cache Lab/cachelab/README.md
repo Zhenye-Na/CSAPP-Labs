f1558bcacf7fe147ec85a55a1270b0efea49f1fb
Zhenye Na <zna2@illinois.edu>
2018-07-04 02:08:15 -0500

    Finished Part B

--- labs/Lab6-Cache Lab/cachelab/README.md
+++ labs/Lab6-Cache Lab/cachelab/README.md
@@ -87,3 +87,92 @@ sys	0m0.106s
 ```
 
 ## Part B: Optimizing Matrix Transpose
+
+```
+$ ./test-trans -M 32 -N 32
+
+Function 0 (2 total)
+Step 1: Validating and generating memory traces
+Step 2: Evaluating performance (s=5, E=1, b=5)
+func 0 (Transpose submission): hits:1766, misses:287, evictions:255
+
+Function 1 (2 total)
+Step 1: Validating and generating memory traces
+Step 2: Evaluating performance (s=5, E=1, b=5)
+func 1 (Simple row-wise scan transpose): hits:870, misses:1183, evictions:1151
+
+Summary for official submission (func 0): correctness=1 misses=287
+
+TEST_TRANS_RESULTS=1:287
+```
+
+```
+$ ./test-trans -M 64 -N 64
+
+Function 0 (2 total)
+Step 1: Validating and generating memory traces
+Step 2: Evaluating performance (s=5, E=1, b=5)
+func 0 (Transpose submission): hits:9026, misses:1219, evictions:1187
+
+Function 1 (2 total)
+Step 1: Validating and generating memory traces
+Step 2: Evaluating performance (s=5, E=1, b=5)
+func 1 (Simple row-wise scan transpose): hits:3474, misses:4723, evictions:4691
+
+Summary for official submission (func 0): correctness=1 misses=1219
+
+TEST_TRANS_RESULTS=1:1219
+```
+
+```
+$ ./test-trans -M 61 -N 67
+
+Function 0 (2 total)
+Step 1: Validating and generating memory traces
+Step 2: Evaluating performance (s=5, E=1, b=5)
+func 0 (Transpose submission): hits:6332, misses:1847, evictions:1815
+
+Function 1 (2 total)
+Step 1: Validating and generating memory traces
+Step 2: Evaluating performance (s=5, E=1, b=5)
+func 1 (Simple row-wise scan transpose): hits:3756, misses:4423, evictions:4391
+
+Summary for official submission (func 0): correctness=1 misses=1847
+
+TEST_TRANS_RESULTS=1:1847
+
+```
+
+
+## Summary
+
+```
+$ ./driver.py
+Part A: Testing cache simulator
+Running ./test-csim
+                        Your simulator     Reference simulator
+Points (s,E,b)    Hits  Misses  Evicts    Hits  Misses  Evicts
+     3 (1,1,1)       9       8       6       9       8       6  traces/yi2.trace
+     3 (4,2,4)       4       5       2       4       5       2  traces/yi.trace
+     3 (2,1,4)       2       3       1       2       3       1  traces/dave.trace
+     3 (2,1,3)     167      71      67     167      71      67  traces/trans.trace
+     3 (2,2,3)     201      37      29     201      37      29  traces/trans.trace
+     3 (2,4,3)     212      26      10     212      26      10  traces/trans.trace
+     3 (5,1,5)     231       7       0     231       7       0  traces/trans.trace
+     6 (5,1,5)  265189   21775   21743  265189   21775   21743  traces/long.trace
+    27
+
+
+Part B: Testing transpose function
+Running ./test-trans -M 32 -N 32
+Running ./test-trans -M 64 -N 64
+Running ./test-trans -M 61 -N 67
+
+Cache Lab summary:
+                        Points   Max pts      Misses
+Csim correctness          27.0        27
+Trans perf 32x32           8.0         8         287
+Trans perf 64x64           8.0         8        1219
+Trans perf 61x67          10.0        10        1847
+          Total points    53.0        53
+```
--- labs/Lab6-Cache Lab/cachelab/trans.c
+++ labs/Lab6-Cache Lab/cachelab/trans.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,43 +6,123 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+/*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+  if (M == 32 && N == 32)
+  {
+    int a, b, c, d;
+    for (a = 0 ; a < N ; a += 8) {
+      for (b = 0 ; b < M ; b += 8) {
+        for (c = b ; c < b + 8 ; ++c) {
+          for (d = a + c - b ; d >= a ; --d) {
+            B[c][d] = A[d][c];
+          }
+          for (d = a + c - b + 1 ; d < a + 8 ; ++d) {
+            B[c][d] = A[d][c];
+          }
+        }
+      }
+    }
+  }
+
+  if (M == 64 && N == 64)
+  {
+    int tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7, a, b, c;
+    for (a = 0 ; a < N ; a += 8)
+    {
+      for (b = 0 ; b < M ; b += 8)
+      {
+        for (c = b ; c < b + 4 ; ++c)
+        {
+          tmp0 = A[c][a];
+          tmp1 = A[c][a + 1];
+          tmp2 = A[c][a + 2];
+          tmp3 = A[c][a + 3];
+          tmp4 = A[c][a + 4];
+          tmp5 = A[c][a + 5];
+          tmp6 = A[c][a + 6];
+          tmp7 = A[c][a + 7];
+          B[a][c] = tmp0;
+          B[a + 1][c] = tmp1;
+          B[a + 2][c] = tmp2;
+          B[a + 3][c] = tmp3;
+          B[a][c + 4] = tmp4;
+          B[a + 1][c + 4] = tmp5;
+          B[a + 2][c + 4] = tmp6;
+          B[a + 3][c + 4] = tmp7;
+        }
+        for (c = a + 4 ; c < a + 8 ; ++c)
+        {
+          tmp0 = B[c - 4][b + 4];
+          tmp1 = B[c - 4][b + 5];
+          tmp2 = B[c - 4][b + 6];
+          tmp3 = B[c - 4][b + 7];
+
+          B[c - 4][b + 4] = A[b + 4][c - 4];
+          B[c - 4][b + 5] = A[b + 5][c - 4];
+          B[c - 4][b + 6] = A[b + 6][c - 4];
+          B[c - 4][b + 7] = A[b + 7][c - 4];
+
+          B[c][b] = tmp0;
+          B[c][b + 1] = tmp1;
+          B[c][b + 2] = tmp2;
+          B[c][b + 3] = tmp3;
+
+          B[c][b + 4] = A[b + 4][c];
+          B[c][b + 5] = A[b + 5][c];
+          B[c][b + 6] = A[b + 6][c];
+          B[c][b + 7] = A[b + 7][c];
+        }
+      }
+    }
+  }
+
+  if (M == 61 && N == 67)
+  {
+    int a, b, c, d;
+    for (a = 0 ; a < N ; a += 16)
+      for (b = 0 ; b < M ; b += 16)
+        for (c = b ; (c < b + 16) && (c < M) ; ++c)
+          for (d = a ; (d < a + 16) && (d < N) ; ++d)
+            B[c][d] = A[d][c];
+
+  }
+
 }
 
-/* 
+/*
  * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
+ * a simple one below to help you get started.
+ */
 
-/* 
+/*
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
 void trans(int M, int N, int A[N][M], int B[M][N])
 {
-    int i, j, tmp;
+  int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; j++) {
+      tmp = A[i][j];
+      B[j][i] = tmp;
+    }
+  }
 
 }
 
@@ -56,14 +136,14 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
+    registerTransFunction(trans, trans_desc);
 
 }
 
-/* 
+/*
  * is_transpose - This helper function checks if B is the transpose of
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
@@ -81,4 +161,3 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
     }
     return 1;
 }
-
