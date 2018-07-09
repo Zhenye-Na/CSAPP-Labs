b0b5ea9a84a322d7e13d02a8a4492dc4066ec427
Zhenye Na <zna2@illinois.edu>
2018-07-09 00:02:35 -0500

    Add disassemble codes

---
+++ labs/Lab4-Buffer Lab/buflab-handout/Dynamite.d
@@ -0,0 +1,12 @@
+
+Dynamite.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	b8 be 8d 04 08       	mov    $0x8048dbe,%eax
+   5:	89 45 d4             	mov    %eax,-0x2c(%ebp)
+   8:	b8 ec 76 1b 76       	mov    $0x761b76ec,%eax
+   d:	83 ec 04             	sub    $0x4,%esp
+  10:	c3                   	ret    
---
+++ labs/Lab4-Buffer Lab/buflab-handout/Firecracker.d
@@ -0,0 +1,11 @@
+
+Firecracker.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	b8 ec 76 1b 76       	mov    $0x761b76ec,%eax
+   5:	a3 00 d1 04 08       	mov    %eax,0x804d100
+   a:	83 c4 10             	add    $0x10,%esp
+   d:	c3                   	ret    
